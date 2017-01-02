61b3dc7cdaea5d8ed3565a03bfdaa4697cda3398
Tiago <Tiago@172.30.0.42>
2017-01-02 18:24:07 +0000

    Corrections

---
+++ httpMultiThreadedServer/src/httpmultithreadedserver/Get_Request.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package httpmultithreadedserver;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Tiago
+ */
+public class Get_Request{
+    
+    public enum status{UNREAD, WAITING, CONCLUDED};
+    
+    private status state;
+    private long ID;
+    private SocketChannel origin;
+    private Date timeOfCreation;
+    private Path path = null;
+    private String protocol;
+    private int statusCode = 0;
+    
+
+    
+    
+    public Get_Request(String data, long ID){
+        
+        
+        this.ID = ID;                   //Request identifier
+        timeOfCreation = new Date();    //Time at which the request was created
+        state = status.UNREAD;
+                
+        //Request's text processing
+        data = data.substring(data.indexOf(' ')+1, data.indexOf('\r'));
+        protocol = data.substring(data.indexOf(' ')+1);
+        
+        
+        //Get html page location on the server
+        try{
+            path = Paths.get(System.getProperty("user.dir"),Paths.get(data.substring(0, data.indexOf(' '))).toString());
+            System.out.println("Path: " + path.toString());
+            statusCode = 200;  //Success
+            
+        }
+        catch (InvalidPathException e){
+            
+            System.out.println("Page could not be found.\n" + e);
+            statusCode = 404;  //Client Error - Page not found
+            
+        }
+        
+    }
+    
+
+    public Date getTimeOfCreation() {
+        return timeOfCreation;
+    }
+    
+    public void setTimeOfCreation(Date timeOfCreation) {
+        this.timeOfCreation = timeOfCreation;
+    }
+    
+    public Path getPath(){
+        
+        return path;
+    }
+    
+    public Get_Response createResponse(){
+        
+        File htmlpage = new File(path.toString());
+        
+        return new Get_Response(ID, protocol, htmlpage, statusCode);
+        
+    }
+}
---
+++ httpMultiThreadedServer/src/httpmultithreadedserver/Get_Response.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package httpmultithreadedserver;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.channels.SocketChannel;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Tiago
+ */
+public class Get_Response {
+    
+    private long ID;
+    private Date timeOfCreation;
+    private String protocol;
+    private int statusCode = 0;
+    private File htmlPage;
+    private FileReader pageReader;
+    
+    
+    public Get_Response(long ID, String _protocol, File _htmlpage, int statusCode){
+        
+        this.ID = ID;
+        timeOfCreation = new Date();
+        this.statusCode = statusCode;
+        protocol = _protocol;
+        this.htmlPage = _htmlpage;
+        
+        try {
+            pageReader = new FileReader(htmlPage);
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Get_Response.class.getName()).log(Level.SEVERE, null, ex);
+            statusCode = 404;
+        }
+    }    
+    
+
+    public Date getTimeOfCreation() {
+        return timeOfCreation;
+    }
+    
+    public void setTimeOfCreation(Date timeOfCreation) {
+        this.timeOfCreation = timeOfCreation;
+    }
+    
+    public int getStatusCode(){
+        
+        return statusCode;
+        
+    }
+    
+    public long getID(){
+        
+        return ID;
+        
+    }
+    
+    public FileReader getPageReader(){
+        
+        return pageReader;
+        
+    }
+    
+    public String buildHeader(File htmlpage){
+        
+        String statusMessage;
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        
+        if(statusCode == 200){
+            
+            try {
+                
+                return (protocol + ' ' + "OK" + '\n'                            +
+                        "Date: " + timeOfCreation.toString()                    +
+                        "Server: " + InetAddress.getLocalHost().getHostName()   +
+                        "Last-Modified: " + sdf.format(htmlpage.lastModified()) +
+                        "Accept-Ranges: bytes"                                  +
+                        "Content-Length: " + htmlpage.length()                  +
+                        "Connection: Close"                                     +
+                        "Content-Type: text/html; charset=UTF-8");
+                
+            } catch (UnknownHostException ex) {
+                Logger.getLogger(Get_Response.class.getName()).log(Level.SEVERE, null, ex);
+                
+                return (protocol + ' ' + "OK" + '\n'                            +
+                        "Date: " + timeOfCreation.toString()                    +
+                        "Server: "                                              +
+                        "Last-Modified: " + sdf.format(htmlpage.lastModified()) +
+                        "Accept-Ranges: bytes"                                  +
+                        "Content-Length: " + htmlpage.length()                  +
+                        "Connection: Close"                                     +
+                        "Content-Type: text/html; charset=UTF-8");
+            }
+        }
+        else{
+            try {
+                
+                return (protocol + ' ' + "Not found" + '\n'                     +
+                        "Date: " + timeOfCreation.toString()                    +
+                        "Server: " + InetAddress.getLocalHost().getHostName()   +
+                        "Accept-Ranges: bytes"                                  +
+                        "Connection: Close");
+                
+            } catch (UnknownHostException ex) {
+                Logger.getLogger(Get_Response.class.getName()).log(Level.SEVERE, null, ex);
+                
+                return (protocol + ' ' + "Not found" + '\n'                     +
+                        "Date: " + timeOfCreation.toString()                    +
+                        "Server: "                                              +
+                        "Accept-Ranges: bytes"                                  +
+                        "Connection: Close");
+            }
+        }
+        
+        
+        
+        
+    }
+}
+
